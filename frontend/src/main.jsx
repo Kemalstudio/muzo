73ef431ee987305677c7a44aa8653f42fdfd0893
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-19 23:42:09 +0500

    2.85

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,4 +8,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-// If you want to start measuring performance in your app, pass a funct
+// If you want to start measuring performance in your app, pass a func
