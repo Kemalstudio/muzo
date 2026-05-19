59bcf70e8fbf36c61a47d719359f276498d22108
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-19 23:50:04 +0500

    3.5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,4 +8,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-// If you want to start measuring performance in yo
+// If you want to start measuring performance in y
