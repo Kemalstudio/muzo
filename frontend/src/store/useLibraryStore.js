be3a2f77c759b6eb93af05d9231a1e38d1f75484
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-20 23:55:35 +0500

    3.31

---
+++ frontend/src/store/useLibraryStore.js
@@ -0,0 +1,9 @@
+import create from 'zustand'
+
+const useLibraryStore = create((set) => ({
+  items: [],
+  setItems: (items) => set({ items }),
+  addItem: (item) => set((state) => ({ items: [...state.items, item] })),
+}))
+
+export default useLibraryStore
