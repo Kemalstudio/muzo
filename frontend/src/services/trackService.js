ade427301906c40771419d6132bd239188032107
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-20 23:54:37 +0500

    3.28

---
+++ frontend/src/services/trackService.js
@@ -0,0 +1,11 @@
+import api from './api'
+
+export async function fetchTracks() {
+  const response = await api.get('/tracks')
+  return response.data
+}
+
+export async function fetchTrackById(id) {
+  const response = await api.get(`/tracks/${id}`)
+  return response.data
+}
