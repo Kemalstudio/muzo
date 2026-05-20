daec30c37eae42eef040754e1f8e94414fb7ac8b
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-20 23:14:18 +0500

    2.75

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -34,6 +34,11 @@ class Artist extends Model
         return $this->hasMany(Album::class);
     }
 
+    public function songs()
+    {
+        return $this->hasMany(Song::class);
+    }
+
     // Accessors for public URLs
     public function getAvatarUrlAttribute()
     {
