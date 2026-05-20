3393766e3c83395a801706c5ea54cef7824bb7ff
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-20 23:00:17 +0500

    258

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function apiResponse(mixed $data = null, string $message = '', int $status = 200): JsonResponse
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message,
+        ], $status);
+    }
 }
