9bc5c07783cc71c447607c283534158e608b4dfc
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-20 23:04:36 +0500

    2.67

---
+++ app/Models/PlaylistTrack.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class PlaylistTrack extends Pivot
+{
+    protected $table = 'playlist_track';
+
+    protected $fillable = [
+        'playlist_id',
+        'track_id',
+        'position',
+    ];
+}
