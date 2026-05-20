b5e71fef1936432b96e68c868ccc496cbedc3c1b
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-20 23:22:09 +0500

    2.77

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -33,6 +33,11 @@ class Album extends Model
         return $this->hasMany(Track::class);
     }
 
+    public function songs()
+    {
+        return $this->hasMany(Song::class);
+    }
+
     public function getCoverUrlAttribute()
     {
         if (! $this->cover_path) {
@@ -40,45 +45,5 @@ class Album extends Model
         }
 
         return Storage::url($this->cover_path);
-    }
-}
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use App\Models\Artist;
-use App\Models\Track;
-
-class Album extends Model
-{
-    use HasFactory;
-
-    protected $fillable = [
-        'uuid',
-        'artist_id',
-        'title',
-        'slug',
-        'cover_art',
-        'description',
-        'released_at',
-        'track_count',
-        'metadata',
-    ];
-
-    protected $casts = [
-        'released_at' => 'datetime',
-        'metadata' => 'array',
-    ];
-
-    public function artist()
-    {
-        return $this->belongsTo(Artist::class);
-    }
-
-    public function tracks()
-    {
-        return $this->hasMany(Track::class);
     }
 }
