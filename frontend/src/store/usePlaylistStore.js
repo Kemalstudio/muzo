c109d874d80d81f1951881cae8601755edb5605b
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-21 23:58:57 +0500

    4.13

---
+++ frontend/src/store/usePlaylistStore.js
@@ -0,0 +1,58 @@
+import create from 'zustand'
+
+const usePlaylistStore = create((set, get) => ({
+  userPlaylists: [],
+  loading: false,
+  error: null,
+
+  setUserPlaylists: (playlists) => set({ userPlaylists: playlists }),
+
+  addPlaylist: (playlist) =>
+    set((state) => ({
+      userPlaylists: [playlist, ...state.userPlaylists],
+    })),
+
+  updatePlaylist: (id, updatedData) =>
+    set((state) => ({
+      userPlaylists: state.userPlaylists.map((p) =>
+        p.id === id ? { ...p, ...updatedData } : p
+      ),
+    })),
+
+  removePlaylist: (id) =>
+    set((state) => ({
+      userPlaylists: state.userPlaylists.filter((p) => p.id !== id),
+    })),
+
+  addTrackToPlaylist: (playlistId, track) =>
+    set((state) => ({
+      userPlaylists: state.userPlaylists.map((p) =>
+        p.id === playlistId
+          ? {
+              ...p,
+              tracks: [...(p.tracks || []), track],
+            }
+          : p
+      ),
+    })),
+
+  removeTrackFromPlaylist: (playlistId, trackId) =>
+    set((state) => ({
+      userPlaylists: state.userPlaylists.map((p) =>
+        p.id === playlistId
+          ? {
+              ...p,
+              tracks: (p.tracks || []).filter((t) => t.id !== trackId),
+            }
+          : p
+      ),
+    })),
+
+  setLoading: (loading) => set({ loading }),
+
+  setError: (error) => set({ error }),
+
+  getPlaylist: (id) => get().userPlaylists.find((p) => p.id === id),
+}))
+
+export default usePlaylistStore
