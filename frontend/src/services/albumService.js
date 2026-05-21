88ee01851c969800739cf7266740f3e852461f70
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-21 22:07:22 +0500

    3.59

---
+++ frontend/src/services/albumService.js
@@ -0,0 +1,11 @@
+import api from './api'
+
+export async function fetchAlbums() {
+  const response = await api.get('/albums')
+  return response.data
+}
+
+export async function fetchAlbumById(id) {
+  const response = await api.get(`/albums/${id}`)
+  return response.data
+}
