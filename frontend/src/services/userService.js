cec72ebf0b37266146c6c0225a9c54c2d3e6c810
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-21 23:58:43 +0500

    4.12

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,11 @@
+import api from './api'
+
+export async function updateUserProfile(userId, formData) {
+  const response = await api.patch(`users/${userId}`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+
+  return response.data.data
+}
