4ec85f8dc672f08f573c4da830d4a87151d6dbe4
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-21 22:07:03 +0500

    3.58

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import AppLayout from '../components/layout/Layout'
 import HomePage from '../pages/Home'
+import ExplorePage from '../pages/Explore'
+import SearchPage from '../pages/Search'
+import ArtistPage from '../pages/Artist'
+import AlbumPage from '../pages/Album'
+import PlaylistPage from '../pages/Playlist'
+import GenrePage from '../pages/Genre'
 import LibraryPage from '../pages/Library'
 import LoginPage from '../pages/Login'
 import RegisterPage from '../pages/Register'
@@ -12,6 +18,12 @@ export default function AppRoutes() {
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="explore" element={<ExplorePage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="artist/:id" element={<ArtistPage />} />
+        <Route path="album/:id" element={<AlbumPage />} />
+        <Route path="playlist/:id" element={<PlaylistPage />} />
+        <Route path="genre/:id" element={<GenrePage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
 
