2bfbd153dda3e59b0dca7c07c665358c1c62d132
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-21 22:07:33 +0500

    3.60

---
+++ frontend/src/services/artistService.js
@@ -0,0 +1,11 @@
+import api from './api'
+
+export async function fetchArtists() {
+  const response = await api.get('/artists')
+  return response.data
+}
+
+export async function fetchArtistById(id) {
+  const response = await api.get(`/artists/${id}`)
+  return response.data
+}
