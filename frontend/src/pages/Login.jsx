e082d950e50fd8621e049161c1e1b4eb289f749d
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-22 21:26:43 +0500

    4.39

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { login } from '../services/authService'
+import { login } from '../services'
 import useAuthStore from '../store/useAuthStore'
 
 export default function LoginPage() {
