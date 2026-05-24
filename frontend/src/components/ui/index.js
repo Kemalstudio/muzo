407864d17df43fb559a15fde76013f62cb261d52
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-24 16:41:31 +0500

    5.78

--- frontend/src/components/ui/index.js
+++ frontend/src/components/ui/index.js
@@ -8,6 +8,8 @@ export { default as SearchBar } from './SearchBar'
 export { default as SectionHeading } from './SectionHeading'
 export { default as TrackCard } from './TrackCard'
 export { default as TrendingSongCard } from './TrendingSongCard'
+export { default as Alert } from './Alert'
+export { default as Accordion } from './Accordion'
 
 
 
